2f27f1cc8db374798cb072705fbea87b40b21cb5
Julian Rodemann <julian.rodemann@freenet.de>
2022-08-03 16:42:31 +0200

    ...

--- playground.R
+++ playground.R
@@ -6,47 +6,47 @@ source("R/makeMBOInfillCritUACB.R")
 source("R/initCrit.InfillCritUACB.R")
 source("R/ShapleyMBO.R")
 
-#fun = smoof::makeAlpine02Function(1)
-fun = smoof::makeAlpine01Function(2)
+fun = smoof::makeAlpine02Function(1)
+#fun = smoof::makeAlpine01Function(2)
 
 var_function = function(x) 0.2*abs(x-10)
 obj_fun = function(x) {
   rnorm(1, mean = fun(x), sd = var_function(x) %>% sqrt())
 }
-obj_fun = makeSingleObjectiveFunction(name = "noisy_parable", 
+obj_fun = makeSingleObjectiveFunction(name = "noisy parable", 
                                       fn = obj_fun, has.simple.signature = TRUE,
-                                      par.set = makeNumericParamSet("x", 1, -10, 10), noisy = TRUE)
+                                      par.set = makeNumericParamSet("x", 1, 0, 10), noisy = TRUE)
 
-percent.noise = 0.05
-# approximate the sd of the function
-noise.free.fun = smoof::makeHyperEllipsoidFunction(dimensions = 4L)
-sample = ParamHelpers::generateDesign(n = 10000, par.set = getParamSet(noise.free.fun), fun = lhs::randomLHS)
-values = apply(sample, 1, noise.free.fun)
-sd = sd(values)
-# sd of the noise is 5% of the estimated sd of the function (moderate noise)
-noise.sd = percent.noise * sd
-# define noisy Objective Function
-obj_fun = smoof::makeSingleObjectiveFunction(
-  name = "noisy 4d Hyper-Ellipsoid",
-  id = "hyper_ellipsoid_4d_5%noise",
-  description = "4d Hyper-Ellipsoid with artificially added gaussian noise.
-    The sd of the noise is 5% of sd of the noise free HypEll, eps ~ N(0, 0.05 * sd(nf.fun))",
-  fn = function(x, sd = noise.sd) { #see makeHyperEllipsoidFunction() of the smoof package
-    n = length(x)
-    eps = rnorm(1, 0, sd) # Gaussian noise
-    sum(1:n * x^2) + eps # the fist term is taken from the source code of makeHyperEllipsoidFunction()
-  }, 
-  par.set = makeNumericParamSet(
-    len = 4, id = "x", # 4 dimensional 
-    lower = rep(-5.12, 4), upper = rep(5.12, 4),
-    vector = TRUE),
-  global.opt.params = rep(0, 4),
-  global.opt.value = 0,
-  noisy = TRUE
-)
+# percent.noise = 0.05
+# # approximate the sd of the function
+# noise.free.fun = smoof::makeHyperEllipsoidFunction(dimensions = 4L)
+# sample = ParamHelpers::generateDesign(n = 10000, par.set = getParamSet(noise.free.fun), fun = lhs::randomLHS)
+# values = apply(sample, 1, noise.free.fun)
+# sd = sd(values)
+# # sd of the noise is 5% of the estimated sd of the function (moderate noise)
+# noise.sd = percent.noise * sd
+# # define noisy Objective Function
+# obj_fun = smoof::makeSingleObjectiveFunction(
+#   name = "noisy 4d Hyper-Ellipsoid",
+#   id = "hyper_ellipsoid_4d_5%noise",
+#   description = "4d Hyper-Ellipsoid with artificially added gaussian noise.
+#     The sd of the noise is 5% of sd of the noise free HypEll, eps ~ N(0, 0.05 * sd(nf.fun))",
+#   fn = function(x, sd = noise.sd) { #see makeHyperEllipsoidFunction() of the smoof package
+#     n = length(x)
+#     eps = rnorm(1, 0, sd) # Gaussian noise
+#     sum(1:n * x^2) + eps # the fist term is taken from the source code of makeHyperEllipsoidFunction()
+#   }, 
+#   par.set = makeNumericParamSet(
+#     len = 4, id = "x", # 4 dimensional 
+#     lower = rep(-5.12, 4), upper = rep(5.12, 4),
+#     vector = TRUE),
+#   global.opt.params = rep(0, 4),
+#   global.opt.value = 0,
+#   noisy = TRUE
+# )
 
 # visualize the function
-autoplot(obj_fun, length.out = 1000)
+autoplot(obj_fun, length.out = 400)
 
 
 budget = 25
